07716943c0668b79d190678c3bcd0f73b657f70e
Yúri Faro <yurifarod@yahoo.com.br>
2022-07-24 23:40:48 -0300

    Add files via upload

--- K_variability_analysis.py
+++ K_variability_analysis.py
@@ -115,8 +115,7 @@ for it in range(25):
     beta_1 = 0.97
     beta_2 = 0.97
     decay  = 0.05
-    epochs = 50
-    #epochs = 250
+    epochs = 250
     
     classificador = Sequential()
     classificador.add(Dense(units = neurons, activation = activation, 
