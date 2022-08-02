bff7c2e672d64eadf2f81efe97021b8f63c0fb7e
Yúri Faro <yurifarod@yahoo.com.br>
2022-08-02 18:42:18 -0300

    Add files via upload

--- L_statistc_analysis.py
+++ L_statistc_analysis.py
@@ -9,14 +9,17 @@ Created on Tue Jul 26 18:56:34 2022
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
-from scipy.stats import friedmanchisquare, kruskal, wilcoxon
+from scipy.stats import kruskal, wilcoxon
 
 classifiers = pd.read_csv(Path('100-fold_cross_validation.csv'), index_col=0)
-
 ann_acc = classifiers["ANN ACC"].values.tolist()
 ann_f1 = classifiers["ANN F1"].values.tolist()
 ens_acc = classifiers["ENS ACC"].values.tolist()
 ens_f1 = classifiers["ENS F1"].values.tolist()
+
+reduced_classifier = pd.read_csv(Path('100-fold_cross_validation_red.csv'), index_col=0)
+red_ens_acc = reduced_classifier["ENS ACC"].values.tolist()
+red_ens_f1 = reduced_classifier["ENS F1"].values.tolist()
 
 #Resolve a bug on the write
 for i in range(100):
@@ -30,20 +33,20 @@ for i in range(100):
         ens_f1[i] = ens_f1[i]/1000
 
 print('ACCURACY')
-print(kruskal(ann_acc, ens_acc))
-print(wilcoxon(ann_acc, ens_acc))
+print(kruskal(red_ens_acc, ann_acc))
+print(wilcoxon(red_ens_acc, ann_acc))
 
 print('F1-SCORE')
-print(kruskal(ann_f1, ens_f1))
-print(wilcoxon(ann_f1, ens_f1))
+print(kruskal(red_ens_f1, ann_f1))
+print(wilcoxon(red_ens_f1, ann_f1))
 
   
 # Creating plot 
-dt_acc = ([ann_acc, ens_acc])
-plt.boxplot(dt_acc)
+# dt_acc = ([ann_acc, red_ens_acc])
+# plt.boxplot(dt_acc)
 
-# dt_f1 = ([ann_f1, ens_f1])
-# plt.boxplot(dt_f1) 
+dt_f1 = ([ann_f1, red_ens_f1])
+plt.boxplot(dt_f1) 
   
 # show plot 
 plt.show()
